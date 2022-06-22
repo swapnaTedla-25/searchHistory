94bdfddee0dd786c1f2f302738db0d0db4a188f5
Swapna <tedlaswapna25@gmail.com>
2022-06-22 16:57:34 +0000

    deleteAdded

--- src/Components/HistoryItem/index.js
+++ src/Components/HistoryItem/index.js
@@ -1,27 +1,58 @@
 import './index.css'
 
 const HistoryItem = props => {
-  const {historyList} = props
-  const {timeAccessed, logoUrl, title, domainUrl} = historyList
+  const {historyList, deleteHistoryItem} = props
+  const {timeAccessed, logoUrl, title, domainUrl, id} = historyList
+
+  const onDelete = () => {
+    deleteHistoryItem(id)
+  }
 
   return (
     <li className="history-item-container">
       <p className="date-time">{timeAccessed}</p>
-      <img src={logoUrl} alt="logoUrl" className="image" />
-      <div className="title-container">
-        <>
-          <p className="title">{title}</p>
-          <p className="domain-url">{domainUrl}</p>
-        </>
-        <div className="delete-button" type="button">
+      <div className="logo-title-url-container">
+        <div className="logo1-title-url-container">
+          <img src={logoUrl} alt="domain logo" className="image" />
+          <div className="title-url-container">
+            <p className="title">{title}</p>
+            <p className="domainUrl">{domainUrl}</p>
+          </div>
+        </div>
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDelete}
+          testid="delete"
+        >
           <img
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
             alt="delete"
             className="delete-icon"
           />
-        </div>
+        </button>
       </div>
     </li>
+    /* <li className="history-item-container">
+      <div className="title-container">
+        <p className="date-time">{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" className="image" />
+        <p className="title">{title}</p>
+        <p className="domainUrl">{domainUrl}</p>
+      </div>
+      <button
+        className="delete-button"
+        type="button"
+        onClick={onDelete}
+        testid="delete"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
+    </li> */
   )
 }
 
--- src/Components/SearchHistory/index.js
+++ src/Components/SearchHistory/index.js
@@ -1,24 +1,116 @@
 import {Component} from 'react'
-import HistoryItem from 'src/Components/HistoryItem'
+import HistoryItem from '../HistoryItem'
 import './index.css'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
 
 class SearchHistory extends Component {
   state = {
+    searchInput: '',
     historyList: initialHistoryList,
   }
 
-  render() {
+  onChangeHistory = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteHistoryItem = id => {
     const {historyList} = this.state
-    const {initialHistoryList} = this.props
+    const updatedList = historyList.filter(eachItem => eachItem.id !== id)
+    this.setState({historyList: updatedList})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {historyList} = this.state
+
+    const historyResults = historyList.filter(eachItem =>
+      eachItem.title.toLowerCase().includes(searchInput),
+    )
 
     return (
       <div className="app-container">
         <div className="history-header-section">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
-            alt="applogo"
-            className="app-logo"
-          />
+          <div className="image-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+              alt="app logo"
+              className="app-logo"
+            />
+          </div>
           <div className="search-input-container">
             <img
               src="https://assets.ccbp.in/frontend/react-js/search-img.png"
@@ -26,18 +118,27 @@ class SearchHistory extends Component {
               className="search-icon"
             />
             <input
-              type="text"
-              value=""
+              type="search"
+              value={searchInput}
               placeholder="search history"
               className="search-input"
+              onChange={this.onChangeHistory}
             />
           </div>
         </div>
         <div className="history-body-section">
           <ul className="history-list-container">
-            {historyList.map(eachItem => (
-              <HistoryItem historyList={eachItem} />
-            ))}
+            {historyResults.length >= 1 ? (
+              historyResults.map(eachItem => (
+                <HistoryItem
+                  historyList={eachItem}
+                  deleteHistoryItem={this.deleteHistoryItem}
+                  key={eachItem.id}
+                />
+              ))
+            ) : (
+              <p>There is No History to Show</p>
+            )}
           </ul>
         </div>
       </div>
